2d23b0aaff4a65076a6254df9b72da4de7cabdba
Pasha <genius.of.simplicity@gmail.com>
2016-10-30 13:22:51 +0100

    implement initialize and add_chessman with specs

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,50 @@
-class Board
+require "king.rb"
+require "queen.rb"
+require "rook.rb"
+require "bishop.rb"
+require "knight.rb"
+require "pawn.rb"
 
+class Board
+	def initialize
+		@board = {}				
+		("a".."h").each do |i|
+			pawn_w = Pawn.new("white")
+			@board.add_chessman([1, i], pawn_w)
+			pawn_b = Pawn.new("black")
+			@board.add_chessman([6, 7 - i], pawn_b)					
+		end
+		["a", "h"].each do |i|
+			rook_w = Rook.new("white")
+			@board.add_chessman([0, i], rook_w)
+			rook_b = Rook.new("black")
+			@board.add_chessman([7, i], rook_b)
+		end
+		["b", "g"].each do |i|
+			knight_w = Knight.new("white")
+			@board.add_chessman([0, i], knight_w)
+			knight_b = Knight.new("black")
+			@board.add_chessman([7, i], knight_b)
+		end
+		["c", "f"].each do |i|
+			bishop_w = Bishop.new("white")
+			@board.add_chessman([0, i], bishop_w)
+			bishop_b = Bishop.new("black")
+			@board.add_chessman([7, i], bishop_b)
+		end
+		queen_w = Queen.new("white")
+		@board.add_chessman([0, "d"], queen_w)
+		queen_b = Queen.new("black")
+		@board.add_chessman([7, "d"], queen_b)
+		king_w = King.new("white")
+		@board.add_chessman([0, "e"], king_w)
+		king_b = King.new("black")
+		@board.add_chessman([7, "e"], king_b)		
+	end
+
+	private
+
+	def add_chessman(position, chessman)
+		@board[position] = chessman
+	end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,5 +1,67 @@
 require "board.rb"
+require "king.rb"
+require "queen.rb"
+require "rook.rb"
+require "bishop.rb"
+require "knight.rb"
+require "pawn.rb"
 
-describe Board do	
+describe Board do
+	describe ".new" do
+		context "setting up board" do
+			let(:get_board_values){ Board.new.instance_variable_get(:@board) }
+			let(:values_init_setup) do
+				board = {}				
+				("a".."h").each do |i|
+					pawn_w = Pawn.new("white")
+					board.send(:add_chessman, [1, i], pawn_w)
+					pawn_b = Pawn.new("black")
+					board.send(:add_chessman, [6, 7 - i], pawn_b)					
+				end
+				["a", "h"].each do |i|
+					rook_w = Rook.new("white")
+					board.send(:add_chessman, [0, i], rook_w)
+					rook_b = Rook.new("black")
+					board.send(:add_chessman, [7, i], rook_b)
+				end
+				["b", "g"].each do |i|
+					knight_w = Knight.new("white")
+					board.send(:add_chessman, [0, i], knight_w)
+					knight_b = Knight.new("black")
+					board.send(:add_chessman, [7, i], knight_b)
+				end
+				["c", "f"].each do |i|
+					bishop_w = Bishop.new("white")
+					board.send(:add_chessman, [0, i], bishop_w)
+					bishop_b = Bishop.new("black")
+					board.send(:add_chessman, [7, i], bishop_b)
+				end
+				queen_w = Queen.new("white")
+				board.send(:add_chessman, [0, "d"], queen_w)
+				queen_b = Queen.new("black")
+				board.send(:add_chessman, [7, "d"], queen_b)
+				king_w = King.new("white")
+				board.send(:add_chessman, [0, "e"], king_w)
+				king_b = King.new("black")
+				board.send(:add_chessman, [7, "e"], king_b)
+				board
+			end
+
+			it { expect(get_board_values).to be_an_instance_of Hash }			
+			it { expect(get_board_values.size).to eql(32) }
+			it { expect(get_board_values).to eql(values_init_setup) }
+		end
+	end
+
+	describe "#add_chessman" do
+		let(:pawn){ double("pawn") }
+		let(:board_with_one_piece) do			
+			board = Board.new
+			board.instance_variable_set(:@board, {})
+			board.send(:add_chessman, 1, "h", pawn)
+			board
+		end
+		it { expect(board_with_one_piece.instance_variable_get(:@board)).to eql({[1, "h"] => pawn}) }
+	end
 	
 end
