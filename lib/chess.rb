fc4d18b73adb302861a66a0bcd6402f4128f8df4
Pasha <genius.of.simplicity@gmail.com>
2016-10-30 10:24:55 +0100

    add Chess.setup method and its spec

--- lib/chess.rb
+++ lib/chess.rb
@@ -1,3 +1,12 @@
+require "player.rb"
+require "board.rb"
+
 class Chess
+	def self.setup
+		board = Board.new
+		player1 = Player.create
+		player2 = Player.create
+		self.new(board, player1, player2)
+	end
 
 end
--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -1,5 +1,29 @@
 require "chess.rb"
+require "player.rb"
+require "board.rb"
 
-describe Chess do	
-	
+describe Chess do
+	describe ".setup" do
+		context "instantiating itself" do			
+			let(:game){ Chess.setup }
+
+			it do
+				expect(Chess).to receive(:new).once
+				game
+			end
+
+			it do
+				expect(game).to be_an_instance_of(Chess)
+			end
+
+			it "setting up players" do						
+				expect(game.instance_variable_get(:@player1)).to be_an_instance_of(Player)
+				expect(game.instance_variable_get(:@player2)).to be_an_instance_of(Player)
+			end
+
+			it "setting up board" do
+				expect(game.instance_variable_get(:@board)).to be_an_instance_of(Board)
+			end
+		end
+	end	
 end
