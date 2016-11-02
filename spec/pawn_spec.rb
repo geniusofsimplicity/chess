c6a2e80e2b6237bad95d1fab1ea7bc10be42deb9
Pasha <genius.of.simplicity@gmail.com>
2016-11-02 18:19:21 +0100

    add spec for pawn and rook

--- spec/pawn_spec.rb
+++ spec/pawn_spec.rb
@@ -3,40 +3,145 @@ require "chess.rb"
 
 describe Pawn do
 	describe "#valid_move?"	do
-		let(:pawn){ Pawn.new("white") }
-		let(:move_from){ [1, "f"] }
-		let(:board) do
-			board = Board.new
-			board.instance_variable_set(:@board, {})		
-			board.send(:add_chessman, move_from, pawn)
-			board
-		end
-		let(:board_values){ board.instance_variable_get(:@board) }
-		context "performing validly" do			
-			let(:move_to){ [2, "f"] }
-			let(:move_to_special){ [3, "f"] }
+		let(:pawn_w){ Pawn.new("white") }
+		let(:pawn_b){ Pawn.new("black") }
 
-			it "a normal move" do				
-				expect(pawn.valid_move?(move_from, move_to, board_values)).to be_truthy
+		context "for white pawn" do
+			let(:move_from){ [1, "f"] }
+			let(:board) do
+				board = Board.new
+				board.instance_variable_set(:@board, {})		
+				board.send(:add_chessman, move_from, pawn_w)
+				board
 			end
-			it "a first special move" do				
-				expect(pawn.valid_move?(move_from, move_to_special, board_values)).to be_truthy
+			let(:board_values){ board.instance_variable_get(:@board) }
+			context "performing validly" do			
+				let(:move_to){ [2, "f"] }
+				let(:move_to_special){ [3, "f"] }	
+				let(:pos_b_1){ [2, "e"] }			
+				let(:pos_b_2){ [2, "g"] }
+				let(:move_to_capture_b_1){ [2, "e"] }	
+				let(:move_to_capture_b_2){ [2, "g"] }	
+				let(:board_w_1) do
+					board = Board.new
+					board.instance_variable_set(:@board, {})		
+					board.send(:add_chessman, pos_b_1, pawn_b)
+					board
+				end
+				let(:board_w_2) do
+					board = Board.new
+					board.instance_variable_set(:@board, {})		
+					board.send(:add_chessman, pos_b_2, pawn_b)
+					board
+				end
+				let(:board_values_w_1){ board_w_1.instance_variable_get(:@board) }
+				let(:board_values_w_2){ board_w_2.instance_variable_get(:@board) }
+
+				it "a normal move" do				
+					expect(pawn_w.valid_move?(move_from, move_to, board_values)).to be_truthy
+				end
+				it "a first special move" do				
+					expect(pawn_w.valid_move?(move_from, move_to_special, board_values)).to be_truthy
+				end
+				it "capturing enemy to the left" do				
+					expect(pawn_w.valid_move?(move_from, move_to_capture_b_1, board_values_w_1)).to be_truthy				
+				end
+				it "capturing enemy to the right" do
+					expect(pawn_w.valid_move?(move_from, move_to_capture_b_2, board_values_w_2)).to be_truthy
+				end
+			end
+			context "performing invalidly" do			
+				let(:move_to_3){ [4, "f"] }
+				let(:move_to_back){ [0, "f"] }
+				let(:move_to_side){ [1, "g"] }
+				let(:move_to_diag_1){ [2, "g"] }
+				let(:move_to_diag_2){ [2, "e"] }
+
+				it "a 3 square move" do				
+					expect(pawn_w.valid_move?(move_from, move_to_3, board_values)).to be_falsey
+				end
+				it "a backward move" do				
+					expect(pawn_w.valid_move?(move_from, move_to_back, board_values)).to be_falsey
+				end
+				it "a side move" do				
+					expect(pawn_w.valid_move?(move_from, move_to_side, board_values)).to be_falsey
+				end
+				it "a right diagonal move" do				
+					expect(pawn_w.valid_move?(move_from, move_to_diag_1, board_values)).to be_falsey
+				end
+				it "a left diagonal move" do				
+					expect(pawn_w.valid_move?(move_from, move_to_diag_2, board_values)).to be_falsey
+				end
 			end
 		end
-		context "performing invalidly" do			
-			let(:move_to_3){ [4, "f"] }
-			let(:move_to_back){ [0, "f"] }
-			let(:move_to_side){ [1, "g"] }
-			
-			it "a 3 square move" do				
-				expect(pawn.valid_move?(move_from, move_to_3, board_values)).to be_falsey
+
+		context "for black pawn" do
+			let(:move_from){ [6, "c"] }
+			let(:board) do
+				board = Board.new
+				board.instance_variable_set(:@board, {})		
+				board.send(:add_chessman, move_from, pawn_b)
+				board
 			end
-			it "a backward move" do				
-				expect(pawn.valid_move?(move_from, move_to_back, board_values)).to be_falsey
+			let(:board_values){ board.instance_variable_get(:@board) }
+			context "performing validly" do			
+				let(:move_to){ [5, "c"] }
+				let(:move_to_special){ [4, "c"] }	
+				let(:pos_w_1){ [5, "b"] }			
+				let(:pos_w_2){ [5, "d"] }
+				let(:move_to_capture_w_1){ [5, "b"] }	
+				let(:move_to_capture_w_2){ [5, "d"] }	
+				let(:board_b_1) do
+					board = Board.new
+					board.instance_variable_set(:@board, {})		
+					board.send(:add_chessman, pos_w_1, pawn_w)
+					board
+				end
+				let(:board_b_2) do
+					board = Board.new
+					board.instance_variable_set(:@board, {})		
+					board.send(:add_chessman, pos_w_2, pawn_w)
+					board
+				end
+				let(:board_values_b_1){ board_b_1.instance_variable_get(:@board) }
+				let(:board_values_b_2){ board_b_2.instance_variable_get(:@board) }
+
+				it "a normal move" do				
+					expect(pawn_b.valid_move?(move_from, move_to, board_values)).to be_truthy
+				end
+				it "a first special move" do				
+					expect(pawn_b.valid_move?(move_from, move_to_special, board_values)).to be_truthy
+				end
+				it "capturing enemy to the left" do				
+					expect(pawn_b.valid_move?(move_from, move_to_capture_w_1, board_values_b_1)).to be_truthy				
+				end
+				it "capturing enemy to the right" do
+					expect(pawn_b.valid_move?(move_from, move_to_capture_w_2, board_values_b_2)).to be_truthy
+				end
 			end
-			it "a side move" do				
-				expect(pawn.valid_move?(move_from, move_to_side, board_values)).to be_falsey
+			context "performing invalidly" do			
+				let(:move_3_squares_down){ [3, "c"] }
+				let(:move_to_back){ [7, "c"] }
+				let(:move_to_side){ [6, "b"] }
+				let(:move_to_diag_1){ [5, "b"] }
+				let(:move_to_diag_2){ [5, "d"] }
+
+				it "moving 3 square down" do				
+					expect(pawn_b.valid_move?(move_from, move_3_squares_down, board_values)).to be_falsey
+				end
+				it "a backward move" do				
+					expect(pawn_b.valid_move?(move_from, move_to_back, board_values)).to be_falsey
+				end
+				it "a side move" do				
+					expect(pawn_b.valid_move?(move_from, move_to_side, board_values)).to be_falsey
+				end
+				it "a left diagonal move" do				
+					expect(pawn_b.valid_move?(move_from, move_to_diag_1, board_values)).to be_falsey
+				end
+				it "a right diagonal move" do				
+					expect(pawn_b.valid_move?(move_from, move_to_diag_2, board_values)).to be_falsey
+				end
 			end
-		end
+		end		
 	end
 end
--- spec/rook_spec.rb
+++ spec/rook_spec.rb
@@ -1,5 +1,87 @@
 require "rook.rb"
+require "pawn.rb"
 
-describe Rook do	
-	
+describe Rook do
+	describe "#valid_move?"	do
+		let(:rook_w){ Rook.new("white") }
+		let(:rook_b){ Rook.new("black") }
+		let(:rook){ [rook_b, rook_w].sample }
+
+		let(:move_from){ [rand(2) + 3, ["d", "e"].sample] }
+		let(:board) do
+			board = Board.new
+			board.instance_variable_set(:@board, {})		
+			board.send(:add_chessman, move_from, rook)
+			board
+		end
+		let(:board_values){ board.instance_variable_get(:@board) }
+
+		context "performing validly" do
+			let(:move_in_rank_up_to){ [ rand(2) + 6, move_from[1]] }
+			let(:move_in_rank_down_to){ [ rand(3), move_from[1]] }
+			let(:move_in_file_left_to){ [ move_from[0], (97 + rand(3)).chr] }
+			let(:move_in_file_right_to){ [ move_from[0], (97 + rand(3) + 5).chr] }
+
+			it "a rank move up" do				
+				expect(rook.valid_move?(move_from, move_in_rank_up_to, board_values)).to be_truthy
+			end
+			it "a rank move down" do				
+				expect(rook.valid_move?(move_from, move_in_rank_down_to, board_values)).to be_truthy
+			end
+			it "a file move to the left" do				
+				expect(rook.valid_move?(move_from, move_in_file_left_to, board_values)).to be_truthy
+			end
+			it "a file move to the right" do				
+				expect(rook.valid_move?(move_from, move_in_file_right_to, board_values)).to be_truthy
+			end
+		end
+
+		context "performing invalidly" do
+			let(:move_in_rank_up_to){ [ rand(2) + 6, move_from[1]] }
+			let(:move_in_rank_down_to){ [ rand(2), move_from[1]] }
+			let(:move_in_file_left_to){ [ move_from[0], (97 + rand(2)).chr] }
+			let(:move_in_file_right_to){ [ move_from[0], (97 + rand(2) + 6).chr] } # landing on g or h
+			let(:board_leap_rank_up) do
+				board = Board.new
+				board.instance_variable_set(:@board, {})		
+				board.send(:add_chessman, [ move_in_rank_up_to[0] - 1, move_in_rank_up_to[1]], Pawn.new(["black", "white"].sample))
+				board
+			end
+			let(:board_value_leap_rank_up){ board_leap_rank_up.instance_variable_get(:@board) }
+			let(:board_leap_rank_down) do
+				board = Board.new
+				board.instance_variable_set(:@board, {})		
+				board.send(:add_chessman, [ move_in_rank_down_to[0] + 1, move_in_rank_down_to[1]], Pawn.new(["black", "white"].sample))
+				board
+			end
+			let(:board_value_leap_rank_down){ board_leap_rank_down.instance_variable_get(:@board) }
+			let(:board_leap_file_left) do
+				board = Board.new
+				board.instance_variable_set(:@board, {})		
+				board.send(:add_chessman, [ move_in_file_left_to[0], (move_in_file_left_to[1].ord + 1).chr], Pawn.new(["black", "white"].sample))
+				board
+			end
+			let(:board_value_leap_file_left){ board_leap_file_left.instance_variable_get(:@board) }
+			let(:board_leap_file_right) do
+				board = Board.new
+				board.instance_variable_set(:@board, {})		
+				board.send(:add_chessman, [ move_in_file_right_to[0], (move_in_file_right_to[1].ord - 1).chr], Pawn.new(["black", "white"].sample))
+				board
+			end
+			let(:board_value_leap_file_right){ board_leap_file_right.instance_variable_get(:@board) }
+
+			it "a leaping rank move up" do				
+				expect(rook.valid_move?(move_from, move_in_rank_up_to, board_value_leap_rank_up)).to be_falsey
+			end
+			it "a leaping rank move down" do				
+				expect(rook.valid_move?(move_from, move_in_rank_down_to, board_value_leap_rank_down)).to be_falsey
+			end
+			it "a file move to the left" do				
+				expect(rook.valid_move?(move_from, move_in_file_left_to, board_value_leap_file_left)).to be_falsey
+			end
+			it "a file move to the right" do				
+				expect(rook.valid_move?(move_from, move_in_file_right_to, board_value_leap_file_right)).to be_falsey
+			end
+		end
+	end	
 end
